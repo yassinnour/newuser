4cad81f028afbb72eed231de07cd06a2dae6e952
yassinnour <72645421+yassinnour@users.noreply.github.com>
2021-01-01 06:37:09 -0600

    my commit

--- src/main/java/page/BasePage.java
+++ src/main/java/page/BasePage.java
@@ -1,5 +1,9 @@
 package page;
 
-public class BasePage {
+import util.BrowserFactory;
 
+public class BasePage {
+	public void User_is_On_the_Techfios_Login_Page() {
+		System.out.println("my name");
+	}
 }
